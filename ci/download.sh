423301eafc4cde99e2ead9acc6dc78492dd723b8
Ivan Krutov <vania-pooh@aerokube.com>
2024-01-24 17:12:32 +0300

    Downloading cypress-moon binaries using Github API

--- ci/download.sh
+++ ci/download.sh
@@ -5,15 +5,12 @@ VERSION=$1
 
 mkdir -p dist
 cd dist
-for osArch in "linux_amd64" "darwin_amd64" "darwin_arm64" "windows_amd64" "windows_386"; do
-  os=$(echo "$osArch" | awk -F "_" '{print $1;}')
-  file='cypress-moon_'"$osArch"
-  downloadURL="https://github.com/aerokube/moon2/releases/download/$VERSION/cypress-moon_darwin_amd64"
-  outputFile="cypress-moon_$osArch"
-  if [ "$os" == "windows" ]; then
-    downloadURL="$downloadURL.exe"
-    outputFile="$outputFile.exe"
-  fi
-  curl -L -o "$outputFile" -s "$downloadURL"
-  chmod 755 "$outputFile"
+
+assetsURL=$(curl -s -H 'Accept: application/vnd.github+json' -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/aerokube/moon2/releases/tags/$VERSION" | jq -r '.assets_url')
+
+curl -s -H 'Accept: application/vnd.github+json' -H "Authorization: Bearer $GITHUB_TOKEN" "$assetsURL" | jq -r '.[] | .name + " " + .url' | while read line; do
+    outputFile=$(echo "$line" | awk '{print $1;}')
+    downloadURL=$(echo "$line" | awk '{print $2;}')
+    curl -L -H 'Accept: application/octet-stream' -H "Authorization: Bearer $GITHUB_TOKEN" -o "$outputFile" -s "$downloadURL"
+    chmod 755 "$outputFile"
 done
